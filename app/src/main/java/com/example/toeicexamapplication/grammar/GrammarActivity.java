78ff017809c2eb3e4ef86493a83826a3e1052ba2
TuongThai <tuongthai28@gmail.com>
2022-05-31 22:25:09 +0700

    Tuong

--- app/src/main/java/com/example/toeicexamapplication/grammar/GrammarActivity.java
+++ app/src/main/java/com/example/toeicexamapplication/grammar/GrammarActivity.java
@@ -2,15 +2,32 @@ package com.example.toeicexamapplication.grammar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.toeicexamapplication.R;
+import com.example.toeicexamapplication.reading.ReadingActivity;
+import com.example.toeicexamapplication.reading.activity_reading_questions;
 
 public class GrammarActivity extends AppCompatActivity {
 
+    private LinearLayout layout_HTD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grammar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.cPrimary)));
+        layout_HTD = findViewById(R.id.layout_HTD);
+        layout_HTD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(GrammarActivity.this, Grammar_List.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/toeicexamapplication/listening/ListeningActivity.java
+++ app/src/main/java/com/example/toeicexamapplication/listening/ListeningActivity.java
@@ -2,15 +2,32 @@ package com.example.toeicexamapplication.listening;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.toeicexamapplication.R;
+import com.example.toeicexamapplication.reading.ReadingActivity;
+import com.example.toeicexamapplication.reading.activity_reading_questions;
 
 public class ListeningActivity extends AppCompatActivity {
 
+    private TextView Let;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listening);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.cPrimary)));
+        Let = findViewById(R.id.textView);
+        Let.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ListeningActivity.this, Listening_Quiz.class);
+                startActivity(intent);
+            }
+        });
     }
 }
